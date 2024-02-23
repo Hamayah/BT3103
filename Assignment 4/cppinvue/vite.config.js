1a4826155266db563a2d045b6d1d0a455f8eee4b
Nicholas Wong <e0727219@u.nus.edu>
2024-02-23 17:22:35 +0800

    Installed dependencies

--- Assignment 4/cppinvue/vite.config.js
+++ Assignment 4/cppinvue/vite.config.js
@@ -1,16 +1,32 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },  
+
+  optimizeDeps: {
+    esbuildOptions: {
+        // Node.js global to browser globalThis
+        define: {
+            global: 'globalThis'
+        },
+        // Enable esbuild polyfill plugins
+        plugins: [
+            NodeGlobalsPolyfillPlugin({
+                process: true,
+                buffer: true
+            }),
+            NodeModulesPolyfillPlugin()
+        ]
+    }
+}
 })
